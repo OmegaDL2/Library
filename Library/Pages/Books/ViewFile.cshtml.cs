411bf1ae228a8a639c602adcc41bf75677c7d287
OmegaDL2 <67266743+OmegaDL2@users.noreply.github.com>
2024-05-12 18:01:01 +0200

    view file saved

---
+++ Library/Pages/Books/ViewFile.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library.Pages.Books
+{
+    public class ViewFileModel : PageModel
+    {
+
+        [BindProperty]
+        public string id { get; set; }
+
+
+        public IActionResult OnGet(string? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            //string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", id);
+            //return File(System.IO.File.OpenRead(filePath), "application/pdf", id);
+
+            string physicalPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", id);
+            byte[] pdfBytes = System.IO.File.ReadAllBytes(physicalPath);
+            MemoryStream ms = new MemoryStream(pdfBytes);
+            return new FileStreamResult(ms, "application/pdf");
+            
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
